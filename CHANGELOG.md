051721a986a88e1cfcd78421105f07eb60e2df58
Eric Herot <eric.github@herot.com>
2013-08-20 14:14:47 -0400

    Do not log to both syslog and haproxy.log

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -1,3 +1,7 @@
+## 1.0.3:
+
+* Don't log haproxy messages to the regular syslog
+
 ## 1.0.2:
 
 * Added changelog
--- metadata.rb
+++ metadata.rb
@@ -4,6 +4,6 @@ maintainer_email 'eric.herot@evertrue.com'
 license          'All rights reserved'
 description      'Installs/Configures et_haproxy'
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version          '1.0.2'
+version          '1.0.3'
 
 depends 'openssl'
