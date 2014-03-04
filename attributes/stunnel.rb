3cc6ca25393114b138ffc832dbd214843978df0e
Jeff Byrnes <jeff@evertrue.com>
2014-03-04 16:52:48 -0500

    Fix FC019: Access node attributes in a consistent manner
    We use strings in other attributes in the cookbook, thus should use strings
    here.

--- attributes/stunnel.rb
+++ attributes/stunnel.rb
@@ -1,5 +1,5 @@
 # coding=utf-8
 
-set[:stunnel][:certificate_path] = '/etc/ssl/certs/STAR.evertrue.com.pem'
-set[:stunnel][:key_path] = '/etc/ssl/private/evertrue.key'
-set[:stunnel][:client_mode] = false
+set['stunnel']['certificate_path'] = '/etc/ssl/certs/STAR.evertrue.com.pem'
+set['stunnel']['key_path'] = '/etc/ssl/private/evertrue.key'
+set['stunnel']['client_mode'] = false
