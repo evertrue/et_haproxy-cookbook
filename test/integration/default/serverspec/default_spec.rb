aa56630619a38bba1c2e2b8b5993813161aa4596
Eric Herot <eric.github@herot.com>
2014-05-09 16:24:20 -0400

    Testing a lot more haproxy.cfg content

--- test/integration/default/serverspec/default_spec.rb
+++ test/integration/default/serverspec/default_spec.rb
@@ -1,6 +1,17 @@
 # Encoding: utf-8
 require 'spec_helper'
 
+describe 'Platform' do
+  describe group('haproxy') do
+    it { should exist }
+  end
+
+  describe user('haproxy') do
+    it { should exist }
+    it { should belong_to_group 'haproxy' }
+  end
+end
+
 describe 'HAProxy Service' do
   describe package('haproxy') do
     it { should be_installed }
@@ -21,15 +32,24 @@ describe 'HAProxy Service' do
     it { should be_file }
     it { should contain 'frontend main' }
     it { should contain 'frontend main_ssl' }
-  end
-
-  describe group('haproxy') do
-    it { should exist }
-  end
-
-  describe user('haproxy') do
-    it { should exist }
-    it { should belong_to_group 'haproxy' }
+    it { should contain 'block if host_stage-api uri_search or host_test_host' }
+    it do
+      should contain 'block if host_stage-api uri_search ' \
+        '!src_access_control_set_global !src_access_control_eips ' \
+        '!src_access_control_instance_ext_ips'
+    end
+    it do
+      should contain 'acl src_access_control_set_global ' \
+        'hdr_ip(X-Forwarded-For) 192.168.19.56 192.168.19.57'
+    end
+    it do
+      should contain 'acl host_stage-api hdr_beg(host) -i ' \
+        'stage-api stage-api.evertrue.com'
+    end
+    it do
+      should contain(%Q(backend legacyapi-stage
+server stage-api-1 stage-api-1.priv.evertrue.com:8080 check))
+    end
   end
 
   describe file('/var/run/haproxy.socket') do
@@ -42,13 +62,9 @@ describe 'HAProxy Service' do
   end
 end
 
-describe 'HAProxy Configuration' do
-  describe command('curl ') do
-    it 'does something' do
-
-    end
-  end
-end
+# describe 'HAProxy Configuration' do
+#   file('/etc/haproxy/haproxy.cfg')
+# end
 
 describe 'haproxyctl' do
   describe package 'ruby1.9.1' do
