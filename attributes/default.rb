908c9f8b8871932929ce02c2735e7b5d9d31491e
Eric Herot <eric.github@herot.com>
2015-01-14 18:19:38 -0500

    Make the inclusion of access control IPs in the haproxy config file optional

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,5 +1,5 @@
 # Encoding: utf-8
-default['haproxy']['access_control']['sets'] = %w(global local)
+default['haproxy']['access_control']['sets'] = []
 default['haproxy']['aws_api_user'] = 'Ec2Haproxy'
 default['haproxy']['syslog']['dest'] = '/dev/log'
 default['haproxy']['syslog']['facility'] = 'local0'
@@ -21,3 +21,6 @@ default['haproxy']['stats'] = {
 }
 default['haproxy']['monitor_uri'] = '/status'
 set['authorization']['sudo']['include_sudoers_d'] = true
+
+default['haproxy']['access_control']['use_eips'] = true
+default['haproxy']['access_control']['use_instance_ips'] = true
